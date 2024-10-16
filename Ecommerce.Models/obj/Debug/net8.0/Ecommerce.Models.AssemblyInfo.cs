f71100ac339f2b40fe921761f9059eb1c505c23e
omeragamy20 <153401997+omeragamy20@users.noreply.github.com>
2024-10-16 23:40:15 +0300

    new merge mona on margn

--- Ecommerce.Models/obj/Debug/net8.0/Ecommerce.Models.AssemblyInfo.cs
+++ Ecommerce.Models/obj/Debug/net8.0/Ecommerce.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11ed01fae09ac255b3b2721c1c9865f697e482b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
