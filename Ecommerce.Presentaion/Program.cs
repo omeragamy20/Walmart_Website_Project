1b436a709b24a6449218c3a730d6e2c91ae6afc2
mahmoud-homosany <mahmoudhomosany6@gmail.com>
2024-10-16 21:18:42 +0300

    Update In Order 16/10/2024

--- Ecommerce.Presentaion/Program.cs
+++ Ecommerce.Presentaion/Program.cs
@@ -77,7 +77,6 @@ namespace Ecommerce.Presentaion
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-                pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
 
             app.Run();
