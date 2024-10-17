71778aec77a42a544030aa224c5dd656d739d7f2
omeragamy20 <153401997+omeragamy20@users.noreply.github.com>
2024-10-17 16:16:10 +0300

    add favorit anf rate work Agamy

---
+++ Ecommerce.Application/Services/FavortandRateService/IFavoriteServices.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.DTOs.DTOsFavoritandRate;
+using Ecommerce.DTOs.shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Services.FavortandRateService
+{
+    public interface IFavoriteServices
+    {
+        public Task<ResultView<CreateorUpdateFavoritDTO>> AddToFavoritAsync(CreateorUpdateFavoritDTO Entity);
+        public Task<ResultView<CreateorUpdateFavoritDTO>> DeleteFavoritAsync(CreateorUpdateFavoritDTO Entity);
+        public Task<List<GetAllFavoritDTO>> AllFavoritbycustomer(string userid);
+        public Task<List<GetAllFavoritDTO>> ThemostFavoritproducts();
+
+
+    }
+}
