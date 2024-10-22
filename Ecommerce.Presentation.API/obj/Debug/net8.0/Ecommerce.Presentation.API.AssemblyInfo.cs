82527283a536db3092703f27ef1b7c79bd6a4bd8
omeragamy20 <153401997+omeragamy20@users.noreply.github.com>
2024-10-22 21:45:30 +0300

    ..

--- Ecommerce.Presentation.API/obj/Debug/net8.0/Ecommerce.Presentation.API.AssemblyInfo.cs
+++ Ecommerce.Presentation.API/obj/Debug/net8.0/Ecommerce.Presentation.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Presentation.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b101aa435d2c700945bd99e9ab3c062c00e2a277")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Presentation.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Presentation.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
