d587c1b6615e7321195ad920ea4dee3d1c555aad
ahmedbahgat <ahmed.bahgat1606@gmail.com>
2024-10-29 16:02:41 +0300

    ay 7aga

--- Ecommerce.Models/obj/Debug/net8.0/Ecommerce.Models.AssemblyInfo.cs
+++ Ecommerce.Models/obj/Debug/net8.0/Ecommerce.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b101aa435d2c700945bd99e9ab3c062c00e2a277")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
