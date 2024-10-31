1dc2633f9696162c5e70b8ef47b89222ecf28b20
mona <monaabdellah91@gmail.com>
2024-10-31 18:01:54 +0300

    .

--- Ecommerce.Presentation.API/obj/Debug/net8.0/Ecommerce.Presentation.API.AssemblyInfo.cs
+++ Ecommerce.Presentation.API/obj/Debug/net8.0/Ecommerce.Presentation.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Presentation.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d99b4ebd507e8422cb515ffbd192ca40c84f485f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7559915ac58f8bc92776ce1ba64f69c5bca18952")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Presentation.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Presentation.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
