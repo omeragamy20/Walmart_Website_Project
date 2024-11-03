e8ed218dcfe198fc76263433d477c3ee8eed4ea2
omeragamy20 <153401997+omeragamy20@users.noreply.github.com>
2024-11-04 01:57:24 +0200

    margn work backend 4/11

--- Ecommerce.Application/obj/Debug/net8.0/Ecommerce.Application.AssemblyInfo.cs
+++ Ecommerce.Application/obj/Debug/net8.0/Ecommerce.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9700dad48a6123a45324d44de0add135dc0c0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7aebb89a31eab9da5e8c9fc770607f58fcac271")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Ecommerce.Presentation.API/obj/Debug/net8.0/Ecommerce.Presentation.API.AssemblyInfo.cs
+++ Ecommerce.Presentation.API/obj/Debug/net8.0/Ecommerce.Presentation.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Presentation.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9700dad48a6123a45324d44de0add135dc0c0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7aebb89a31eab9da5e8c9fc770607f58fcac271")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Presentation.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Presentation.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
