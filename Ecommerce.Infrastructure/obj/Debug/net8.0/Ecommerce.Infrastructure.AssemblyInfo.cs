2c03dec6ca9842c8bc4f1ab79ba8502b7566ec37
omeragamy20 <153401997+omeragamy20@users.noreply.github.com>
2024-11-14 16:37:28 +0200

    margn and mona

--- Ecommerce.Infrastructure/obj/Debug/net8.0/Ecommerce.Infrastructure.AssemblyInfo.cs
+++ Ecommerce.Infrastructure/obj/Debug/net8.0/Ecommerce.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2dc23489ce18236388b6384b201b5ae7426a4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4634c7387a97d939349f9f235c1951dfbc30b358")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
